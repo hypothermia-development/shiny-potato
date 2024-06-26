303e727ca36d8c3df43f14ac8e7aa224663e8865
JoeyMCPC <142174364+JoeyMCPC@users.noreply.github.com>
2024-06-26 16:58:36 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "auth_core.h"
+#include <iostream>
+
+
+int main()
+{
+	
+
+	// main auth loop
+
+
+	while (true)
+	{
+		if (hasAccess = true)
+		{
+		break;
+		}
+		
+
+		doLogin(password, didLogin, num_of_attempts_login);
+		onAttempt(num_of_attempts_login, didLogin)
+	}
+
+	//insert nonsense here
+}
